b16c65aa02a91d647b3bf5ce0387455cad1b807b
= <=>
2019-04-12 09:18:37 -0700

    set up components

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const massive = require('massive')
+
+const { PORT, CONNECTION_STRING } = process.env
+
+app.use(express.json())
+
+massive(CONNECTION_STRING)
+    .then(db => { 
+        app.set('db', db)
+        console.log('connected to db')
+    }).catch(err => { 
+        console.log('DB failed to connect')
+    })
+
+app.listen(PORT, () => { 
+    console.log(`Server started on port ${PORT}`)
+})
