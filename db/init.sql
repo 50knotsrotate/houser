9df802dec2843d49cac8da95f7f0c4073a5d5681
= <=>
2019-04-12 10:16:33 -0700

    set up database with massive, create houses table

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+drop table if exists houses;
+
+create table houses(
+    id serial primary key,
+    name varchar(100),
+    address varchar(100),
+    city varchar(50),
+    state varchar(20),
+    zip integer
+);
+
+-- insert into houses (name, address, city, state, zip)
+-- values("Humble abode", "6 Waterway Way", "Colts Neck", 07722);
+
+insert into houses(name, address, city, state, zip)
+values ('Humble abode', '6 Armistead', 'Colts Neck', 'NJ', 07722 )
--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ app.use(express.json())
 massive(CONNECTION_STRING)
     .then(db => { 
         app.set('db', db)
+        db.init()
+            .then(table => { 
+                console.log('hit')
+            }).catch(err => { 
+                console.log(err)
+            })
         console.log('connected to db')
     }).catch(err => { 
         console.log('DB failed to connect')
