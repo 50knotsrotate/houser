50770b32f601034e7cefb3d0765e1893fd8d4903
= <=>
2019-04-12 11:16:36 -0700

    set up set state handler functions on inputs in Wizard component

--- src/components/Wizard/Wizard.jsx
+++ src/components/Wizard/Wizard.jsx
@@ -6,41 +6,88 @@ export default class Wizard extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            name: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: ''
 
         }
     }
+
+    handlePropertyName = name => {
+        this.setState({
+            name
+        })
+    }
+
+    handleAddressChange = address => { 
+        this.setState({
+            address
+        })
+    }
+
+    cancel = () => { 
+        this.setState({
+            name: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: ''
+        })
+    }
+
+    handleCityChange = city => { 
+        this.setState({
+            city
+        })
+    }
+
+    handleStateChange = state => { 
+        this.setState({
+            state
+        })
+    }
+
+    handleZipChange = zip => {
+        this.setState({
+            zip
+        })
+     }
+
+
 
     render() {
         return (
             <div className='wizard'>
                 <div className='top'>
                     <h1>Add New Listing</h1>
-                    <Link to = '/'> <button className='cancel-button'>Cancel</button></Link>
+                    <Link to = '/'> <button onClick = {this.cancel} className='cancel-button'>Cancel</button></Link>
                 </div>
                 <div className='form'>
                     <div className='form-item'>
                         <h1>Property Name</h1>
-                        <input type = 'text' placeholder = 'What is da name'/>
+                        <input onChange={e => this.handlePropertyName(e.target.value)} value={this.state.name} type = 'text' placeholder = 'What is da name'/>
                     </div>
                     <div className='form-item'>
                         <h1>Address</h1>
-                        <input type = 'text' placeholder = 'Address'/>
+                        <input onChange={e => this.handleAddressChange(e.target.value)} value={this.state.address}type = 'text' placeholder = 'Address'/>
                     </div>
                     <div className='city-state-zip'>
                         
                         <div className='form-item'>
                             <h1>City</h1>
-                            <input type='text' placeholder='City' />
+                            <input onChange={e => this.handleCityChange(e.target.value)} value={this.state.city} type='text' placeholder='City' />
                         </div>
 
                         <div className='form-item'>
                             <h1>State</h1>
-                            <input type='text' placeholder='State' />
+                            <input onChange={e => this.handleStateChange(e.target.value)} value={this.state.state} type='text' placeholder='State' />
                         </div>
 
                         <div className='form-item'>
                             <h1>Zip</h1>
-                            <input type='number' placeholder='Zip' />
+                            <input onChange={e => this.handleZipChange(e.target.value)} value={this.state.zip} type='number' placeholder='Zip' />
                         </div>
                     </div>
                 </div>
